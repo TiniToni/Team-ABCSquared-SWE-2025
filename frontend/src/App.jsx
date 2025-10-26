ded2115ea5eec9203c5c6189e3cdb1b8b8684cd2
andi0316 <andriasubhit@gmail.com>
2025-10-26 19:27:00 -0400

    login ui framework & landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route, Link } from 'react-router-dom';
+import LoginUI from './pages/LoginUI';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='header-container'> 
+        <h2 className='header-title'>ChefTamer!</h2>
+
+        <nav className='navbar'>
+          <div className='link-container'>
+            <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
+          </div>
+
+          <button className='user-icon'></button>
+        </nav>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      {/*routes*/}
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/login" element={<LoginUI />} />
+      </Routes>
     </>
   )
 }
